cc7ac78ee0a169a23f45ebb75e561b7725193c28
Pavel Makarichev <vixtir90@gmail.com>
2017-11-19 20:50:02 +0300

    some visual changes

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ app.get('/', (req,res) => {
   res.sendFile(path.join(`${__dirname}/views/index.html`))
 })
 
-app.get('/register', (req,res) => {
-  res.sendFile(path.join(`${__dirname}/views/signup.html`))
-})
+// app.get('/register', (req,res) => {
+//   res.sendFile(path.join(`${__dirname}/views/signup.html`))
+// })
 
 app.get('/login', (req,res) => {
   res.sendFile(path.join(`${__dirname}/views/login.html`))
