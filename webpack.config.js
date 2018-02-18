f0a5e02a6c8e8d1a7bae3e9e7dd99b900a3e09ca
Pavel Makarichev <vixtir90@gmail.com>
2018-02-18 14:37:42 +0300

    heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
 const path = require('path');
-const ExtractTextPlugin = require ('extract-text-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
   entry: {
     app: './public/scripts/index.js',
-    admin: './public/scripts/admin.js'
+    admin: './public/scripts/admin.js',
   },
   output: {
     path: path.resolve(__dirname, 'public/dist'),
     filename: '[name].bundle.js',
-    library: "myLib",
-    libraryTarget: "umd"
+    library: 'myLib',
+    libraryTarget: 'umd',
   },
   module: {
-      rules:[
-        {
-          test: /\.css$/,
-          use: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: [ 'css-loader' ]
-        })
-        },
-      ]
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader'],
+        }),
+      },
+    ],
   },
   plugins: [
-    new ExtractTextPlugin('bundle.css')
+    new ExtractTextPlugin('bundle.css'),
   ],
-  watch: true
+  watch: false,
 };
 
 module.exports = config;
